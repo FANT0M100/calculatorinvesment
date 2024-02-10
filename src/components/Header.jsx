d10cf0a1203e22ccfe01dacfaed7a67818c8197a
FANT0M100 <giorgie103188@gmail.com>
2024-02-10 12:47:09 +0400

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import headerimg from "../assets/investment-calculator-logo.png";
+
+const Header = () => {
+  return (
+    <header id="header">
+      <img src={headerimg} alt="logo" />
+      <h1>Invesment Calculator</h1>
+    </header>
+  );
+};
+
+export default Header;
